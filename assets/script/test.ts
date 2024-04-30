82b8dc67cdefb7f1018c00db41db39d6064818b4
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-30 14:50:05 +0530

    working

--- assets/script/test.ts
+++ assets/script/test.ts
@@ -1,6 +1,7 @@
 import { _decorator, Component, EditBox, EditBoxComponent, instantiate, Label, Node, Prefab, ScrollView } from "cc";
 import io from "socket.io-client/dist/socket.io.js";
 import { setLabel } from "./setLabel";
+import { send } from "process";
 // import cors from "cors";
 
 const { ccclass, property } = _decorator;
@@ -11,6 +12,12 @@ export class test extends Component {
     scrollView: ScrollView = null;
     @property({ type: Prefab })
     prefab: Prefab;
+
+    @property({ type: EditBoxComponent })
+    editBox: EditBoxComponent = null;
+
+    @property({ type: Label })
+    editBoxTextLabel: Label = null;
 
     click() {}
     start() {
@@ -25,6 +32,18 @@ export class test extends Component {
             node.getComponent(setLabel).setLabelFunc(msg);
             this.scrollView.content.addChild(node);
         });
+        this.editBox.node.on(
+            EditBoxComponent.EventType.EDITING_DID_ENDED,
+            () => {
+                const senderMsg = this.editBoxTextLabel.getComponent(Label).string;
+                const node = instantiate(this.prefab);
+                node.getComponent(setLabel).setLabelFunc(senderMsg);
+                socket.emit("sender message", senderMsg);
+                this.scrollView.content.addChild(node);
+                this.editBoxTextLabel.string = "";
+            },
+            this
+        );
     }
 
     update(deltaTime: number) {}
